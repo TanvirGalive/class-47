39541d701dd91f16b7338c44136f024bf27a63b8
TanvirGalive <galivetanvir@gmail.com>
2017-05-26 23:30:52 +0600

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        $(".testimonial-carousel").owlCarousel({
+            items: 1,
+            loops: true,
+            autoplay: true,
+            nav: false,
+            dots: true
+            
+        });
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
